b7af7326890c77c8bc68d9c286c4339f5ebc92e7
CitraAyu0 <meilindacitraayu@gmail.com>
2024-09-06 19:11:51 +0700

    simpan kode java k github

---
+++ Latihan05.java
@@ -0,0 +1,19 @@
+/**
+ * Latihan05
+ */
+public class Latihan05 {
+
+    public static void main(String[] args) {
+        System.out.println("Nama Saya Citra Ayu Meilinda");
+        System.out.println("Saya adalah mahasiswa baru Jurusan Teknologi Informasi-POLINEMA");
+        System.out.println("Data Mahasiswa Jurusan Teknologi Informasi");
+        System.out.println("Nama    :Citra Ayu Meilinda");
+        System.out.println("NIM     :244107060015");
+        System.out.println("Kelas   :1C");
+        System.out.println("No Absen:05");
+        System.out.println("Prodi   :D4 Sistem Informasi Bisnis");
+        System.out.println("Alamat  :Lingkungan Petungasri Pandaan Pasuruan");
+        System.out.println("No HP   :085850856604");
+        System.out.println("Hobi    :Berenang");
+    }
+}
